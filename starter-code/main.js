38468e50955fd835296ae661c6614faee3702c03
dddotcom <dddotcom@gmail.com>
2017-01-17 17:50:36 -0800

    add createCards dynamically with javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,19 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
-  alert ("You found a match!");
-} else {
-  alert ("Sorry, try again.");
+// if (cardTwo === cardFour) {
+//   alert ("You found a match!");
+// } else {
+//   alert ("Sorry, try again.");
+// }
+
+function createCards(numCards) {
+  for(var i = 0; i < numCards; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = "card";
+    var board = document.getElementById('game-board');
+    board.appendChild(newCard);
+  }
 }
+
+createCards(4);
